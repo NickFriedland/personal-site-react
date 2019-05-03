864401d56945cc709ac9a98de0742f79e8d4baa8
Nick Friedland <nick.friedland@gmail.com>
2019-05-03 16:37:22 -0700

    Created placeholder components for footer

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return <div className='Footer' />;
+  }
+
+  state = {};
+}
+
+Footer.propTypes = {};
+
+Footer.defaultProps = {};
+
+export default Footer;
