0a950253f8db94a442dd943b52962a6860dd888d
Nick Friedland <nick.friedland@gmail.com>
2019-05-16 15:28:51 -0700

    styling updates to portfolio and new assets added

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,79 +1,45 @@
 import React, { Component } from 'react';
 import './Portfolio.css';
-import relish from './images/portfolio_images/relish_homepage.png';
+import relish from './images/portfolio_images/relish_logo.png';
 import langly from './images/portfolio_images/langly.png';
 import subway from './images/portfolio_images/blue.png'
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  CardDeck,
-  Button
-} from 'reactstrap';
 
 class Portfolio extends Component {
   render() {
     return (
-      <div>
+      <div className="portfolio">
         <div className="portfolio-header">
           <h1>Portfolio Projects</h1>
           <h3>Examples of my work from hackathons, passion projects and internships.</h3>
-          <img src={ subway } alt="" srcset=""/>
+          <img src={ subway } alt="" />
         </div>
-        <div>
-          <CardDeck>
-            <Card>
-              <CardImg
-                top
-                src={langly}
-                alt="Card image cap"
-              />
-              <CardBody>
-                <CardTitle>Langly</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </Card>
-            <Card>
-              <CardImg
-                top
-                src={relish}
-                alt="Card image cap"
-              />
-              <CardBody>
-                <CardTitle>Microblog</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </Card>
-            <Card>
-              <CardImg
-                top
-                src={relish}
-                alt="Card image cap"
-              />
-              <CardBody>
-                <CardTitle>Jobly</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </Card>
-          </CardDeck>
+        <div className="portfolio-odd">
+          <img src={langly} alt="" className="portfolio-img col-6"/>  
+          <div className="portfolio-desc col-4">
+            <h3>Lang.ly</h3>
+            <p>
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+            <p>
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div className="portfolio-even">
+          <img src={relish} alt="" className="portfolio-img col-6"/>  
+          <div className="portfolio-desc col-4">
+            <h3>The Relish</h3>
+            <p>
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+            <p>
+              Some quick example text to build on the card title and make up
+              the bulk of the card's content.
+            </p>
+          </div>
         </div>
       </div>
     );
