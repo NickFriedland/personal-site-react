577072e3a736d3bc0b304c902618ce406f9a415b
Nick Friedland <nick.friedland@gmail.com>
2019-05-21 21:31:40 -0700

    Adding css adjustments to portfolio + icons

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 import './Portfolio.css';
 import relish from './images/portfolio_images/relish_logo.png';
 import langly from './images/portfolio_images/langly.png';
-import subway from './images/portfolio_images/blue.png'
+import subway from './images/portfolio_images/blue.png';
+import api from './images/portfolio_images/tech/api.png';
+import d3 from './images/portfolio_images/tech/d3.png';
+import firebase from './images/portfolio_images/tech/firebase.png';
+import flask from './images/portfolio_images/tech/flask.png';
+import heroku from './images/portfolio_images/tech/heroku.png';
+import js from './images/portfolio_images/tech/js.png';
+import css from './images/portfolio_images/tech/css.png';
+import html from './images/portfolio_images/tech/html.png';
+import moment from './images/portfolio_images/tech/moment.js.png';
+import node from './images/portfolio_images/tech/node.png';
+import python from './images/portfolio_images/tech/python.png';
+import react from './images/portfolio_images/tech/react.png';
+import redux from './images/portfolio_images/tech/redux.png';
 
 class Portfolio extends Component {
   render() {
@@ -14,14 +27,27 @@ class Portfolio extends Component {
           <img src={ subway } alt="" />
         </div>
         <div className="project langly">
-          <div className="item langly-item col-6">
-            <img src={langly} alt=""/>
-            <a href="https://github.com/NickFriedland/langly-react-fe" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-github portfolio-github"></i>
-            </a>
-            <a href="https://langly-readability.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-              <i className="far fa-share-square"></i>
-            </a>
+          <div className="col-6 langly-left">
+            <div className="item langly-item">
+              <img src={langly} alt=""/>
+              <a href="https://github.com/NickFriedland/langly-react-fe" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-github portfolio-github"></i>
+              </a>
+              <a href="https://langly-readability.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                <i className="far fa-share-square"></i>
+              </a>
+            </div>
+            <div className="tech-logos langly-logos">
+              <img src={js} alt="" id="js-html-css"/>
+              <img src={css} alt="" id="js-html-css"/>
+              <img src={html} alt="" id="js-html-css"/>
+              <img src={react} alt=""/>
+              <img src={node} alt=""/>
+              <img src={python} alt=""/>
+              <img src={flask} alt=""/>
+              <img src={api} alt=""/>
+              <img src={heroku} alt=""/>
+            </div>
           </div>
           <div className="desc langly-desc col-4">
             <h3 className="title">Langly</h3>
@@ -29,17 +55,29 @@ class Portfolio extends Component {
               This project was the result of a two day hackathon. If given a URL for an article, it generates an estimated readability score, and formats the content for more convenient online reading.
             </p>
             <p>
-              The app makes use of several well-known readability algorithms, npm and python packages, and API integrations. This is a work in progress: desired future functionality includes foreign language support, selecting words to define (or translate) them via a tool tip, and storing vocab as flashcards in a database.
+              The app makes use of several well-known readability algorithms, npm and python packages, and API integrations. Desired future functionality includes foreign language support, selecting words to define (or translate) them via a tool tip, and storing vocab as flashcards in a database.
             </p>
           </div>
         </div>
         <div className="project relish">
-          <div className="item relish-item col-6">
-            <img src={relish} alt="" />
-            <a href="https://www.therelish.com/" target="_blank" rel="noopener noreferrer">
-              <i class="fab fa-medium-m"></i>
-            </a>
-          </div>  
+          <div className="col-6">  
+            <div className="item relish-item">
+              <img src={relish} alt="" />
+              <a href="https://www.therelish.com/" target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-medium-m"></i>
+              </a>
+            </div>
+            <div className="tech-logos relish-logos">
+              <img src={js} alt="" id="js-html-css"/>
+              <img src={css} alt="" id="js-html-css"/>
+              <img src={html} alt="" id="js-html-css"/>
+              <img src={react} alt=""/>
+              <img src={redux} alt=""/>
+              <img src={firebase} alt=""/>
+              <img src={d3} alt=""/>
+              <img src={moment} alt=""/>
+            </div>
+          </div>   
           <div className="desc relish-desc col-4">
             <h3 className="title">The Relish</h3>
             <p>
@@ -49,7 +87,7 @@ class Portfolio extends Component {
               Source code and web app access for this tool are unavailable because it is proprietary. However, I wrote a Medium article that describes in detail my contributions to this tool.
             </p>
           </div>
-        </div>
+        </div>          
       </div>
     );
   }
