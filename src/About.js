04333294c6c31f70429dd84bbfacd1b42d5f7c09
Nick Friedland <nick.friedland@gmail.com>
2019-05-28 17:17:56 -0700

    mobile friendly about section

--- src/About.js
+++ src/About.js
@@ -7,30 +7,29 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-        <div className="about-header">
+        <div className="hero">
           <img src={nick} alt="" className="headshot-img"/>
-          <div className="about-header-desc">
-            <h1 className="about-header-title">Nick Friedland</h1>
-            <h2 className="about-header-subtitle">Full stack software engineer passionate about environmental and social innovation</h2>
+          <div className="hero-desc">
+            <h1 className="hero-title">Nick Friedland</h1>
+            <h2 className="hero-subtitle">Full stack software engineer passionate about environmental and social innovation</h2>
           </div>
         </div>
         <div className="body">
-          <div className="col-6">
-            <h2 className="section-header">About Me</h2>
-            <p className="about-text">
+          <div className="about-section">
+            <h2>About Me</h2>
+            <p>
               I am a full stack software engineer with experience in JavaScript- including React.js, Redux.js, jQuery, Node.js, and Express- as well as backend Python, Flask, and PostgreSQL and frontend HTML5 and CSS3. I'm passionate about making a positive social and environmental impact in my work, and am especially excited about green tech and environmental innovation.
             </p>
-            <p className="about-text">
+            <p>
               In my personal time I enjoy making art, writing, cooking, listening to all kinds of music, language learning, and various other creative pursuits. I’m fluent in Spanish and eager to learn more languages.
             </p>
-            <p className="about-text">
+            <p>
               I’m currently located in Northern California, which is where I grew up, but I love to travel and am open to opportunities and adventures domestically and abroad.
             </p>
           </div>
-          <div className="col-2 contact">
-            <h2 className="section-header">Contact</h2>
-            <div className="about-link-container">
-              
+          <div className="contact-section">
+            <h2>Contact</h2>
+            <div className="icon-container">
               <a
                 href="https://github.com/NickFriedland"
                 rel="noopener noreferrer"
@@ -47,13 +46,6 @@ class About extends Component {
               </a>
               <a href="mailto:nick.friedland@gmail.com">
                 <i className="far fa-envelope" />
-              </a>
-              <a
-                href="https://angel.co/nick-friedland?public_profile=1"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <i className="fab fa-angellist" />
               </a>
             </div>
           </div>
