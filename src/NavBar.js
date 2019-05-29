a1476cfd106dc039396f8bf139d7d7e73c9b1ef9
Nick Friedland <nick.friedland@gmail.com>
2019-05-28 17:18:07 -0700

    Mobile friendly nav

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,34 +1,72 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from './images/NF.png';
 import './NavBar.css';
 import {
   Navbar,
   Nav,
-  NavItem} from 'reactstrap';
+  NavItem,
+  // Collapse,
+  // NavbarToggler,
+  // UncontrolledDropdown,
+  // DropdownMenu,
+  // DropdownItem,
+  // DropdownToggle 
+} from 'reactstrap';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   render() {
-    const activeStyle = {
-      fontSize: '1.5em',
-      color: 'black' 
-    }
     return (
-      <nav>
-        <Navbar expand='md' className='nav'>
-          <NavLink to='/' activeStyle={activeStyle}> Nick Friedland </NavLink>
-          <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink exact to='/' className='normal' activeClassName='active'>About</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink exact to='/Portfolio' className='normal' activeClassName='active'>Portfolio</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink exact to='/Blog' className='normal' activeClassName='active'>Blog</NavLink>
-              </NavItem>
-            </Nav>
-        </Navbar>
-      </nav>
+      <Navbar expand='md'>
+        <NavLink to='/'><span className="nav-text">Nick Friedland</span><img src={logo} alt="" className="nav-logo" /></NavLink>
+        <Nav className="ml-auto md" navbar>
+          <NavItem>
+            <NavLink exact to='/' className='normal' activeClassName='active'>About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink exact to='/Portfolio' className='normal' activeClassName='active'>Portfolio</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink exact to='/Blog' className='normal' activeClassName='active'>Blog</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+          /* <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto md nav-small" navbar>
+            <UncontrolledDropdown nav inNavbar navbar-expand-md>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      <NavLink exact to='/' className='normal' activeClassName='active'>About</NavLink>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <NavLink exact to='/Portfolio' className='normal' activeClassName='active'>Portfolio</NavLink>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <NavLink exact to='/Blog' className='normal' activeClassName='active'>Blog</NavLink>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse> */
     );
   }
 
